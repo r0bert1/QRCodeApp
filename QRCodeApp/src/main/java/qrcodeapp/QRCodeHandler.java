b1e5b5cd1f56c1b9dfa0276d63add8671be6c5be
Robert <robert.pakkanen@helsinki.fi>
2019-04-02 15:54:33 +0300

    update

---
+++ QRCodeApp/src/main/java/qrcodeapp/QRCodeHandler.java
@@ -0,0 +1,67 @@
+package qrcodeapp;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeHandler {
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+    
+    private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+
+    public static void main(String[] args) {
+        /*try {
+            generateQRCodeImage("Hello world!", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }*/
+        
+        try {
+            File file = new File("MyQRCode.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
