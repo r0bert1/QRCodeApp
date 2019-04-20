6d8048146295c78a02ca1173a99e019b8887cc51
Robert <robert.pakkanen@helsinki.fi>
2019-04-20 13:13:30 +0300

    more gui improvements

--- QRCodeApp/src/main/java/fi/pakkaner/gui/QRCodeApplication.java
+++ QRCodeApp/src/main/java/fi/pakkaner/gui/QRCodeApplication.java
@@ -33,7 +33,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Region;
+import javafx.stage.DirectoryChooser;
 
 public class QRCodeApplication extends Application {
 
@@ -42,26 +42,30 @@ public class QRCodeApplication extends Application {
     private ImageView imgWebCamCapturedImage;
     private Label decodedText;
     private Webcam webCam = null;
-    private boolean stopCamera = false;
     private ObjectProperty<Image> imageProperty = new SimpleObjectProperty<>();
 
     @Override
     public void start(Stage primaryStage) {
 
-        primaryStage.setTitle("QRCodeApplication");
+        primaryStage.setTitle("QR Code Application");
+        
+        // DirectoryChooser setup
+        
+        final DirectoryChooser dirChooser = new DirectoryChooser();
+        dirChooser.setTitle("Choose directory");
+        dirChooser.setInitialDirectory(new File(System.getProperty("user.home")));
 
-        // initial scene
+        // Start menu scene
         
         BorderPane root1 = new BorderPane();
         root1.setStyle("-fx-background-color: #ccc;");
-        Insets insets = new Insets(10);
         
         GridPane menuPane = new GridPane();
         menuPane.setHgap(10);
         menuPane.setVgap(12);
+        menuPane.setAlignment(Pos.CENTER);
         
         Label scanLbl = new Label("Scan a QR code");
-        BorderPane.setMargin(scanLbl, insets);
         Button goScanBtn = new Button("Scan");
         
         Label generateLbl = new Label("Generate your own QR code");
@@ -72,96 +76,77 @@ public class QRCodeApplication extends Application {
         menuPane.add(generateLbl, 2, 0);
         menuPane.add(goGenerateBtn, 2, 1);
         
-        BorderPane.setAlignment(menuPane, Pos.CENTER);
-        
-        menuPane.setAlignment(Pos.CENTER);
-        
         root1.setCenter(menuPane);
 
-        // scan scene
+        // Scan scene
         
         root = new BorderPane();
-        root.setPadding(new Insets(10, 10, 10, 10));
-        
-        
-        GridPane scanPane = new GridPane();
-        scanPane.setHgap(10);
-        scanPane.setVgap(12);
+        root.setPadding(new Insets(10));
         
         root.setStyle("-fx-background-color: #ccc;");
         imgWebCamCapturedImage = new ImageView();
-        Label infoLbl = new Label("Decoded text: ");
         decodedText = new Label("No QR code detected yet.");
+        decodedText.setStyle("fx-font-weight: bold");
+        decodedText.setPadding(new Insets(25, 0, 0, 0));
         BorderPane.setAlignment(decodedText, Pos.CENTER);
         
         Button backBtn1 = new Button("Back");
-        
-        BorderPane pane = new BorderPane();
         BorderPane.setAlignment(backBtn1, Pos.BOTTOM_RIGHT);
-        
-        scanPane.add(infoLbl, 0, 0);
-        scanPane.add(decodedText, 1, 0);
-        
-        pane.setCenter(scanPane);
-        pane.setBottom(backBtn1);
-        
-        
-        
-        
-        //scanPane.setAlignment(Pos.CENTER);
         
         root.setCenter(imgWebCamCapturedImage);
         root.setBottom(backBtn1);
         root.setTop(decodedText);
 
-        // generate scene
+        // Generate scene
         
         BorderPane root2 = new BorderPane();
         root2.setPadding(new Insets(10));
-        
-        
         root2.setStyle("-fx-background-color: #ccc;");
-        ImageView generatedImgView = new ImageView();
-        Label imageName = new Label();
         
+        ImageView generatedImgView = new ImageView();
+        
+        Label imageName = new Label();
         BorderPane.setAlignment(imageName, Pos.CENTER);
         
         GridPane imagePropertyPane = new GridPane();
         imagePropertyPane.setHgap(10);
         imagePropertyPane.setVgap(12);
+        imagePropertyPane.setAlignment(Pos.CENTER);
         
         Label textLabel = new Label("Text to encode:");
         TextField textInput = new TextField();
-        
         Label nameLabel = new Label("Filename:");
-        TextField nameInput = new TextField();
-        
+        TextField nameInput = new TextField();  
         Button generateBtn = new Button("Generate");
+        
         Button backBtn2 = new Button("Back");
         BorderPane.setAlignment(backBtn2, Pos.BOTTOM_RIGHT);
         
         BorderPane base = new BorderPane();
+        
+        Label dirLbl = new Label("Target directory:");
+        TextField dirFld = new TextField();
+        Button browseBtn = new Button("Browse");
 
         imagePropertyPane.add(textLabel, 0, 0);
         imagePropertyPane.add(textInput, 1, 0);
         imagePropertyPane.add(nameLabel, 0, 1);
         imagePropertyPane.add(nameInput, 1, 1);
-        imagePropertyPane.add(generateBtn, 1, 2);
-        
-        
-        
-        imagePropertyPane.setAlignment(Pos.CENTER);
+        imagePropertyPane.add(generateBtn, 1, 3);
+        imagePropertyPane.add(dirLbl, 0, 2);
+        imagePropertyPane.add(dirFld, 1, 2);
+        imagePropertyPane.add(browseBtn, 3, 2);
+         
         base.setCenter(imagePropertyPane);
         base.setBottom(backBtn2);
         
         root2.setTop(imageName);
         root2.setCenter(generatedImgView);
         root2.setBottom(base);
-        //root.setBottom(imagePropertyPane);
         
         // scenes
         
-        Scene initial = new Scene(root1);
+        Scene start = new Scene(root1);
         Scene generate = new Scene(root2);
         Scene scan = new Scene(root);
         
@@ -176,34 +161,52 @@ public class QRCodeApplication extends Application {
             primaryStage.setScene(generate);
         });
         
+        browseBtn.setOnAction(e -> {
+            File dir = dirChooser.showDialog(primaryStage);
+            
+            if (dir != null) {
+                dirFld.setText(dir.getAbsolutePath());
+            } else {
+                dirFld.setText(null);
+            }
+        });
+        
         generateBtn.setOnAction(e -> {
             try {
                 String text = textInput.getText();
                 String name = nameInput.getText();
+                String targetDir = dirFld.getText();
                 
-                qrch.generateQRCode(text, 400, 400, "./images/" + name);
-                File file = new File("./images/" + name);
+                qrch.generateQRCode(text, 400, 400, targetDir + "/" + name);
+                File file = new File(targetDir + "/" + name);
                 Image generatedImg = new Image(file.toURI().toString());
                 generatedImgView.setImage(generatedImg);
                 imageName.setText(name);
+                
+                textInput.clear();
+                nameInput.clear();
+                dirFld.clear();
             } catch (Exception ex) {
                 System.out.println("Failed to generate qr code");
             }
         });
         
         backBtn1.setOnAction(e -> {
-            primaryStage.setScene(initial);
+            primaryStage.setScene(start);
             webCam.close();
         });
         
         backBtn2.setOnAction(e -> {
-            primaryStage.setScene(initial);
+            textInput.clear();
+            nameInput.clear();
+            dirFld.clear();
+            primaryStage.setScene(start);
         });
         
         // Setting initial scene
 
-        primaryStage.setScene(initial);
-        primaryStage.setHeight(600);
+        primaryStage.setScene(start);
+        primaryStage.setHeight(700);
         primaryStage.setWidth(700);
         primaryStage.centerOnScreen();
         primaryStage.show();
